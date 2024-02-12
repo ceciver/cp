12c85a6f7ad8743a55d91c6536d300bbc8cb993f
Ibrahim El Mountasser <33915853+ceciver@users.noreply.github.com>
2024-02-13 00:11:51 +0100

    Create bridges.cpp

---
+++ template/bridges.cpp
@@ -0,0 +1,67 @@
+vvi adj;
+vi id;
+vi low;
+
+int n;
+int curr_id;
+vpii bridges;
+
+void dfs(int u, int p) {
+    id[u] = low[u] = ++curr_id;
+    for(int v: adj[u]) {
+        if(v == p) continue;
+        if (id[v] == 0) { // if V has not been discovered before
+            dfs(v, u);  // recursive DFS call
+            if (id[u] < low[v]) // condition to find a bridge
+                bridges.push_back({u, v});
+            low[u] = min(low[u], low[v]); 
+        } else 
+            low[u] = min(low[u], id[v]);
+    }
+
+}
+
+void scc() {
+    id = vi(n); 
+    low = vi(n);
+    curr_id = 0;
+    bridges = vpii();
+
+    for(int i=0;i<n;i++) {
+        if(id[i] == 0) {
+            dfs(i, i);
+        }
+    }
+}
+
+
+void solve() {
+    adj = vvi(n);
+
+    for(int _=0;_<n;_++) {
+        int node; 
+        cin>>node;
+        char cc;
+        cin>>cc;
+        int k;
+        cin>>k;
+        cin>>cc;
+        for(int _=0;_<k;_++) {
+            int v;
+            cin>>v;
+            adj[node].pb(v);
+        }
+    }
+
+
+    scc();
+    for(auto& [u,v]: bridges) {
+        if(u>v) swap(u,v);
+    }
+    sort(all(bridges));
+    cout<<sz(bridges)<<" critical links"<<endl;
+    for(auto [u,v]: bridges) {
+        cout<<u<<" - "<<v<<endl;
+    }
+    cout<<endl;
+}
