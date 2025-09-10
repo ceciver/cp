6875ab81472d233d10c0eca6cac2ee66593f7819
Ibrahim El Mountasser <33915853+ceciver@users.noreply.github.com>
2025-09-10 10:48:24 +0100

    Update FT_min_max.cpp

--- template/FT_min_max.cpp
+++ template/FT_min_max.cpp
@@ -1,19 +1,21 @@
-const int N = 1 << 18; // has to be power of 2
-const int inf = 1 << 28;
-
 struct RMQ {
-    int a[N * 2];
-    RMQ() {
-        for(int i=0;i<N * 2;i++)
-            a[i] = -inf;
+
+    vi a;
+    int N;
+
+    RMQ(int n) {
+        int p = 31 - __builtin_clz(n);
+        N = 1 << (p + 1);
+        a = vi(2 * n, -INF);
     }
+
     void setMax(int pos, int x) {
         for (int i = pos + N; i; i >>= 1)
             a[i] = max(a[i], x);
     }
 
     int getMax(int L, int R) const {
-        int res = -inf;
+        int res = -INF;
         for (L += N, R += N; L < R; L >>= 1, R >>= 1) {
             if (L & 1) {
                 res = max(res, a[L]);
